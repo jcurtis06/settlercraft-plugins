0e1000ccb72ac2c84ba8280aa8040096ea61f0a7
jcurtis06 <06jcurtis@gmail.com>
2022-12-04 16:27:55 -0500

    Removed pay command to Wallet ➡️

---
+++ Wallet/src/main/java/com/settlercraft/wallet/commands/PayCMD.kt
@@ -0,0 +1,4 @@
+package com.settlercraft.wallet.commands
+
+class PayCMD {
+}
--- Wallet/src/main/java/com/settlercraft/wallet/player/PlayerWallet.kt
+++
@@ -1,7 +0,0 @@
-package com.settlercraft.wallet.player
-
-import java.util.UUID
-
-class PlayerWallet(val uuid: UUID, var money: Int) {
-
-}
